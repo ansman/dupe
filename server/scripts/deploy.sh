0031816c3ab86a8fba4542f0737a286409ceb6a5
Nicklas Ansman Giertz <nicklas@ansman.se>
2013-09-29 13:08:22 -0700

    Separate

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,11 +1,27 @@
-pushd .
-cd ../client
-lein cljsbuild once development
-ssh dupe@192.241.196.82 'rm -rf public'
-scp -r public dupe@192.241.196.82:
+#!/usr/bin/env sh
 
-popd
-rm target/*.jar
-lein uberjar
-scp target/server-standalone.jar dupe@192.241.196.82:server.jar
-ssh dupe@192.241.196.82 'sudo supervisorctl restart dupe'
+function build_client {
+  pushd .
+  cd ../client
+  lein cljsbuild clean
+  lein cljsbuild once development
+  ssh dupe@192.241.196.82 'rm -rf public'
+  scp -r public dupe@192.241.196.82:
+  popd
+}
+
+function build_server {
+  rm target/*.jar
+  lein uberjar
+  scp target/server-standalone.jar dupe@192.241.196.82:server.jar
+  ssh dupe@192.241.196.82 'sudo supervisorctl restart dupe'
+}
+
+if [ "$1" = "client" ]; then
+  build_client
+elif [ "$1" = "server" ]; then
+  build_server
+else
+  build_client
+  build_server
+fi
