aff8bb1b3c075377eeb03a8d3e2c547083b10768
Eskil Andreen <eskil.andreen@gmail.com>
2013-09-29 06:14:09 +0200

    Add client to deployment script

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,3 +1,11 @@
+pushd .
+cd ../client
+lein cljsbuild once production
+ssh dupe@192.241.196.82 'rm -rf public'
+scp -r public dupe@192.241.196.82:
+
+popd
+rm target/*.jar
 lein uberjar
 scp target/server-standalone.jar dupe@192.241.196.82:server.jar
 ssh dupe@192.241.196.82 'sudo supervisorctl restart dupe'
